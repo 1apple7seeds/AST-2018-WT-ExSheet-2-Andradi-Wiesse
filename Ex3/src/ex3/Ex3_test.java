2fbf3f1b3f8abb91540f4f299917895e3683e6e0
hkhbrus <hkran85@gmail.com>
2018-11-14 22:12:54 +0100

    created the packages Ex1, Ex2, and Ex3

---
+++ Ex3/src/ex3/Ex3_test.java
@@ -0,0 +1,14 @@
+package ex3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Ex3_test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
