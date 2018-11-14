b3688dbb344342eb92b0b81a2beed1445e3c36ab
hkhbrus <hkran85@gmail.com>
2018-11-14 22:29:08 +0100

    Added skeleteon for Ex3 class and implemented test cases

--- Ex3/src/ex3/Ex3.java
+++ Ex3/src/ex3/Ex3.java
@@ -1,5 +1,17 @@
 package ex3;
 
 public class Ex3 {
-
+	public double function_f(double[] o_values) {
+		// returns the f value for the given o_values
+		return 0;
+	}
+	
+	public double function_f_kn(int k, int n) {
+		// returns the f value based on the given n and k values
+		return 0;
+	}
+	
+	public static void main(String []args) {
+		System.out.println("Testing");
+	}
 }
--- Ex3/src/ex3/Ex3_test.java
+++ Ex3/src/ex3/Ex3_test.java
@@ -3,12 +3,38 @@ package ex3;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import ex3.Ex3;
 
 class Ex3_test {
 
+	Ex3 Ex3;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		Ex3 = new Ex3();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		Ex3 = null;
+	}
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void function_f_test() {
+		assertEquals(0,Ex3.function_f(new double[] {0,0,0}));
+		assertEquals(0.5,Ex3.function_f(new double[] {0.5,0.5,0.5}));
+		assertEquals(0.9986,Ex3.function_f(new double[] {0.9,0.9,0.9}),0.0001);	
+	}
+	
+	@Test
+	void function_f_kn_test() {
+		assertEquals(0.0122,Ex3.function_f_kn(1,3),0.0001);
+		assertEquals(0.0122,Ex3.function_f_kn(2,3),0.0001);	
+		assertEquals(0.5,Ex3.function_f_kn(3,3),0.0001);
+		assertEquals(0.0122,Ex3.function_f_kn(10,20),0.0001);
 	}
 
 }
