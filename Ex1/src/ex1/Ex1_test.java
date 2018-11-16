126f4e965daa419c4541049f0db61a711036171c
1apple7seeds <charliehpwiesse@gmail.com>
2018-11-16 11:01:48 +0100

    tests 1 and 2 template

--- Ex1/src/ex1/Ex1_test.java
+++ Ex1/src/ex1/Ex1_test.java
@@ -2,12 +2,36 @@ package ex1;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class Ex1_test {
+	
+	private Ex1 q1;
 
+	@BeforeEach
+	void setUp() {
+		q1 = new Ex1();
+	}
+
+	@AfterEach
+	void tearDown(){
+		q1 = null;
+	}
+	
 	@Test
-	void test() {
+	void prime_int_test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void time_prime_test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void time_int_test() {
 		fail("Not yet implemented");
 	}
 
